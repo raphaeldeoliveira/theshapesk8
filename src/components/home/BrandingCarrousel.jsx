f3b4831ba9e6014db692f4c38b663a57726c4c96
alunolages <alunolages@lages.sc>
2024-03-11 09:30:12 -0300

    correção de problema nas rotas e inicio das implementações na pagina Home

---
+++ src/components/home/BrandingCarrousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function BradingCarrousel() {
+    
+    
+    return (
+        <div>
+            <h3>left button</h3>
+            <h3>right button</h3>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Header from "../components/global/Header";
 import NavOptions from "../components/home/NavOptions";
+import mainBanner from "../assets/images/home/main_banner.jpg"
+import "../styles/pages/home/homePage.scss"
 
 import "../styles/main.scss"
 
@@ -8,10 +9,11 @@ export default function Home() {
 
 
     return (
-        <div>
-            <Header />
+        <div className="homePage">
             <NavOptions />
-            <h1>Home</h1>
+            <img src={mainBanner} />
+            {/* a imagem a cima tem que direcionar para a rota de busca e passar o parametro "ofertas" na barra de busca pra fazer uma consulta SQL e retornar os items em promoção */}
+            <h1>Home goes here</h1>
         </div>
     )
 }
