99b357f429b254b85353364e0dec9a66760c04f8
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-03-30 21:49:47 -0300

    correção de pequeno bug quando clicava sobre o produto nao rolava a tela pra cima

--- src/components/global/ProductCard.jsx
+++ src/components/global/ProductCard.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/pages/search/productcard.scss";
 
 export default function ProductCard(props) {
+    //const topRef = useRef(null);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
 
     return (
-        <Link to={`/product/${props.id}`} className="productCard">
+        <Link onClick={scrollToTop} to={`/product/${props.id}`} className="productCard">
             <img alt="" src={props.image} />
             <h2>{props.name}</h2>
             <h3>{props.price}</h3>
+            {/* Esta div vazia será a referência para rolar para o topo */}
+            {/*<div ref={topRef} />*/}
         </Link>
     )
 }
