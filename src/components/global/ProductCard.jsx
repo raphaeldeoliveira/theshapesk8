940f971130e079140979ed486a518c2fbbf180dc
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-03-30 21:52:45 -0300

    correção ESLint

--- src/components/global/ProductCard.jsx
+++ src/components/global/ProductCard.jsx
@@ -1,10 +1,9 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/pages/search/productcard.scss";
 
 export default function ProductCard(props) {
-    //const topRef = useRef(null);
-
+    
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
@@ -15,8 +14,6 @@ export default function ProductCard(props) {
             <img alt="" src={props.image} />
             <h2>{props.name}</h2>
             <h3>{props.price}</h3>
-            {/* Esta div vazia será a referência para rolar para o topo */}
-            {/*<div ref={topRef} />*/}
         </Link>
     )
 }
