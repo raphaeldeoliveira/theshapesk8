84b31f8208e46de78d06967e9d8371a8b808cc7e
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-04-04 15:07:41 -0300

    implementação da cor correta das brandings

--- src/components/home/BrandingCarrousel.jsx
+++ src/components/home/BrandingCarrousel.jsx
@@ -8,23 +8,25 @@ import alien from "../../assets/images/home/brandings/Alien-Workshop-HP-Logo-Fad
 import dogtown from "../../assets/images/home/brandings/Dogtown-Skateboards-Brand-Icon-90-2.jpg"
 import vans from "../../assets/images/home/brandings/HomepageBrandLogos_02.jpg"
 import bonies from "../../assets/images/home/brandings/HomepageBrandLogos_05.jpg"
-import dickies from "../../assets/images/home/brandings/HomepageBrandLogos_06.jpg"
-import spitfire from "../../assets/images/home/brandings/HomepageBrandLogos_09.jpg"
+import dickies from "../../assets/images/home/brandings/HomepageBrandLogos_06.png"
+import spitfire from "../../assets/images/home/brandings/HomepageBrandLogos_09.png"
 import creature from "../../assets/images/home/brandings/HomepageBrandLogos_15.jpg"
 import santacruz from "../../assets/images/home/brandings/HomepageBrandLogos_19.jpg"
 import blacklabel from "../../assets/images/home/brandings/HomepageBrandLogos_20.jpg"
-import allbrands from "../../assets/images/home/brandings/AllBrands1.jpg"
+//import allbrands from "../../assets/images/home/brandings/AllBrands1.jpg"
+import toymachine from "../../assets/images/home/brandings/toymachine.jpg"
 // imagens com hover
 import alien_hover from "../../assets/images/home/brandings_hover/Alien-Workshop-HP-Logo-Hover.png"
 import dogtown_hover from "../../assets/images/home/brandings_hover/Dogtown-Skateboards-Brand-Icon.jpg"
 import vans_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_02.jpg"
 import bonies_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_05.jpg"
 import dickies_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_06.jpg"
-import spitfire_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_09.jpg"
-import creature_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_15.jpg"
+import spitfire_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_09.png"
+import creature_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_15.png"
 import santacruz_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_19.jpg"
 import blacklabel_hover from "../../assets/images/home/brandings_hover/HomepageBrandLogos2_20.jpg"
-import allbrands_hover from "../../assets/images/home/brandings_hover/AllBrands2.jpg"
+//import allbrands_hover from "../../assets/images/home/brandings_hover/AllBrands2.jpg"
+import toymachine_hover from "../../assets/images/home/brandings_hover/toymachine_hover.jpg"
 
 export default function BradingCarrousel() {
     
@@ -32,8 +34,8 @@ export default function BradingCarrousel() {
 
     // aqui vao ser colcadas as imagens ja importadas
     const brandingName = ["Alien", "Dogtown", "Vans", "Bonies", "Dickies", "Spitfire", "Creature", "Santacruz", "Blacklabel", "Allbrands"] 
-    const branding = [alien, dogtown, vans, bonies, dickies, spitfire, creature, santacruz, blacklabel, allbrands]
-    const brandingHover = [alien_hover, dogtown_hover, vans_hover, bonies_hover, dickies_hover, spitfire_hover, creature_hover, santacruz_hover, blacklabel_hover, allbrands_hover]
+    const branding = [alien, dogtown, vans, bonies, dickies, spitfire, creature, santacruz, blacklabel, toymachine]
+    const brandingHover = [alien_hover, dogtown_hover, vans_hover, bonies_hover, dickies_hover, spitfire_hover, creature_hover, santacruz_hover, blacklabel_hover, toymachine_hover]
 
     return (
         <div className="brandingCarrousel">
