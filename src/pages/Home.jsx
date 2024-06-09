f0cbc7bd2ecd8ddc55e93b87990c3354c77a914e
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-06-09 16:28:10 -0300

    correção de bugs do esline

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import NavOptions from "../components/home/NavOptions";
-import mainBanner from "../assets/images/home/banners/main_banner.jpg"
 import BradingCarrousel from "../components/home/BrandingCarrousel";
 import SearchPageGrid from "../components/global/SearchPageGrid";
 import BannerCarrousel from "../components/home/BannerCarrousel";
-import { useNavigate } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 
 import "../styles/pages/home/homePage.scss";
@@ -12,14 +10,12 @@ import "../styles/main.scss";
 
 export default function Home() {
 
-    const navigate = useNavigate()
     const { t } = useTranslation();
 
     return (
         <div className="homePage">
             <NavOptions />
             <BannerCarrousel />
-            {/*<img onClick={() => navigate("/search/Spitfire")} alt="" src={mainBanner} />*/}
             <BradingCarrousel />
             <SearchPageGrid h1title={t('pageGridTitle')} />
         </div>
