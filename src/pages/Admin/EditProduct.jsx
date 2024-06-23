56b1195cb0dbe2e5976a8b80b3f63b3cdc95cb50
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-06-23 14:53:08 -0300

    função de editar produto implementado

--- src/pages/Admin/EditProduct.jsx
+++ src/pages/Admin/EditProduct.jsx
@@ -2,39 +2,46 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import LoadingSpinner from "../../components/global/LoadingSpinner";
 import { useTranslation } from 'react-i18next';
+import "../../styles/pages/admin/editProduct.scss";
 
 export default function EditProduct() {
 
     const { t } = useTranslation();
     
-    const [loading, setLoading] = useState(true)
-    const productId = useParams();
-    console.log("productId")
-    console.log(productId.productid)
-
+    const [loading, setLoading] = useState(true);
+    const { productid } = useParams(); // Destructuring para obter productid diretamente
     const [formData, setFormData] = useState({
+        id: "",
         nome: "",
         descricao: "",
-        imagem: "",
         valor: 0,
-        tamanho: ""
+        categoria: "",
+        subcategoria: "",
+        marca: "",
+        images: [],
+        products: []  // Renomeado para manter consistência com a estrutura de dados
     });
 
     useEffect(() => {
-        if (productId.productid) {
+        if (productid) {
             async function fetchProduct() {
                 try {
-                    setLoading(true)
-                    const response = await fetch(`https://e-commerce-prod.onrender.com/api/produtos/${productId.productid}`);
+                    setLoading(true);
+                    const response = await fetch(`http://localhost:8080/mainProductTeste/${productid}`);
                     if (response.ok) {
                         const productData = await response.json();
-                        console.log(productData)
+                        console.log("productData");
+                        console.log(productData);
                         setFormData({
-                            nome: productData.dados.nome,
-                            descricao: productData.dados.descricao,
-                            imagem: productData.dados.imagem,
-                            valor: productData.dados.valor,
-                            tamanho: productData.dados.tamanho
+                            id: productData.productDetail.id,
+                            nome: productData.productDetail.nome,
+                            descricao: productData.productDetail.descricao,
+                            valor: productData.productDetail.valor,
+                            categoria: productData.productDetail.categoria,
+                            subcategoria: productData.productDetail.subcategoria,
+                            marca: productData.productDetail.marca,
+                            images: productData.images,
+                            products: productData.products  // Ajustado para o campo products
                         });
                     } else {
                         console.error('Falha ao recuperar os dados do produto');
@@ -42,79 +49,101 @@ export default function EditProduct() {
                 } catch (error) {
                     console.error('Erro ao conectar à API:', error);
                 } finally {
-                    setLoading(false)
+                    setLoading(false);
                 }
             }
             fetchProduct();
         } else {
             setFormData({
+                id: "",
                 nome: "",
                 descricao: "",
-                imagem: "",
                 valor: 0,
-                tamanho: ""
+                categoria: "",
+                subcategoria: "",
+                marca: "",
+                images: [],
+                products: []
             });
-            setLoading(false)
+            setLoading(false);
         }
-    }, [productId]);      
+    }, [productid]);      
 
-    const handleChange = (e) => {
+    const handleChange = (e, index, type) => {
         const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+        if (type === 'images') {
+            const updatedImages = [...formData.images];
+            updatedImages[index][name] = value;
+            setFormData({ ...formData, images: updatedImages });
+        } else if (type === 'products') {  // Renomeado para manter consistência
+            const updatedProducts = [...formData.products];
+            updatedProducts[index][name] = value;
+            setFormData({ ...formData, products: updatedProducts });
+        } else {
+            setFormData({ ...formData, [name]: value });
+        }
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            setLoading(true)
-            const response = await fetch(`https://e-commerce-prod.onrender.com/api/produtos/${productId.productid}`, {
+            setLoading(true);
+            const response = await fetch(`http://localhost:8080/mainProductTeste`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(formData)
+                body: JSON.stringify({
+                    productDetail: {
+                        id: formData.id,
+                        nome: formData.nome,
+                        descricao: formData.descricao,
+                        valor: formData.valor,
+                        categoria: formData.categoria,
+                        subcategoria: formData.subcategoria,
+                        marca: formData.marca
+                    },
+                    products: formData.products.map(item => ({
+                        id: item.id || null,  // Incluir id se necessário para atualização
+                        tamanho: item.tamanho,
+                        quantidade: item.quantidade
+                    })),
+                    images: formData.images.map(item => ({
+                        id: item.id || null,  // Incluir id se necessário para atualização
+                        imagem: item.imagem
+                    }))
+                })
             });
             if (response.ok) {
-                alert(t('updateProductAlertMsg'))
+                alert(t('updateProductAlertMsg'));
                 console.log('Produto atualizado com sucesso!');
             } else {
-                alert(t('failedUpdatedMsg'))
+                alert(t('failedUpdatedMsg'));
                 console.error('Falha ao atualizar o produto');
             }
         } catch (error) {
             console.error('Erro ao conectar à API:', error);
+            alert(t('failedUpdatedMsg'));
         } finally {
-            setLoading(false)
+            setLoading(false);
         }
     };
 
     if (loading) {
-        return <LoadingSpinner verticalsize="500" horizontalsize="800" />
+        return <LoadingSpinner verticalsize="500" horizontalsize="800" />;
     } else {
         return (
             <div className="product__edit">
                 <h1>{t('editProduct')}</h1>
-                <form className="product__edit" onSubmit={handleSubmit}>
-                    <div className="edit__half--left">
+                <form className="product__edit__container" onSubmit={handleSubmit}>
+                    <div className="product__edit__details">
                         <label>
                             {t('name')}:
                             <input type="text" name="nome" value={formData.nome} onChange={handleChange} />
                         </label>
                         <br />
                         <label>
-                            {t('imagem')}:
-                            <input type="text" name="imagem" value={formData.imagem} onChange={handleChange} />
-                        </label>
-                        <br />
-                        <label>
-                            {t('tamanho')}:
-                            <input type="text" name="tamanho" value={formData.tamanho} onChange={handleChange} />
-                        </label>
-                        <br />
-                    </div>
-                    <div className="edit__hald--right">
-                        <label>
-                            {t('description')}:
+                            {t('descricao')}:
                             <input type="text" name="descricao" value={formData.descricao} onChange={handleChange} />
                         </label>
                         <br />
@@ -123,13 +152,52 @@ export default function EditProduct() {
                             <input type="number" name="valor" value={formData.valor} onChange={handleChange} />
                         </label>
                         <br />
-                        <button type="submit">{t('updateProduct')}</button>
+                        <label>
+                            {t('categoria')}:
+                            <input type="text" name="categoria" value={formData.categoria} onChange={handleChange} />
+                        </label>
+                        <br />
+                        <label>
+                            {t('subcategoria')}:
+                            <input type="text" name="subcategoria" value={formData.subcategoria} onChange={handleChange} />
+                        </label>
+                        <br />
+                        <label>
+                            {t('marca')}:
+                            <input type="text" name="marca" value={formData.marca} onChange={handleChange} />
+                        </label>
+                        <br />
                     </div>
+                    <div className="product__edit__images">
+                        {formData.images.map((item, index) => (
+                            <div key={index}>
+                                <label>
+                                    {t('imagem')} {index + 1}:
+                                    <input type="text" name="imagem" value={item.imagem} onChange={(e) => handleChange(e, index, 'images')} />
+                                </label>
+                                <br />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="product__edit__quantity-and-size">
+                        {formData.products.map((item, index) => (
+                            <div key={index}>
+                                <label>
+                                    {t('tamanho')} {index + 1}:
+                                    <input type="text" name="tamanho" value={item.tamanho} onChange={(e) => handleChange(e, index, 'products')} />
+                                </label>
+                                <br />
+                                <label>
+                                    {t('quantidade')} {index + 1}:
+                                    <input type="number" name="quantidade" value={item.quantidade} onChange={(e) => handleChange(e, index, 'products')} />
+                                </label>
+                                <br />
+                            </div>
+                        ))}
+                    </div>
+                    <button type="submit">{t('updateProduct')}</button>
                 </form>
             </div>
-            
         );
-    }
-
-    
+    }    
 }
