783461502b9a111786b363125b6a8c9016509749
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-06-27 19:57:14 -0300

    correção de bug causado pelo ESLint

--- src/pages/RecoverPassword/RecoverTemplate.jsx
+++ src/pages/RecoverPassword/RecoverTemplate.jsx
@@ -71,7 +71,7 @@ export default function RecoverTemplate(props) {
         loading ? (
             <LoadingSpinner />
         ) : (
-            <div className="recover-password__background" style={{ display: props.currentRecoverPage === props.id ? "" : "none" }}>
+            <div className="recover-password__background" style={{ display: String(props.currentRecoverPage) === props.id ? "" : "none" }}>
                 <div className="recover-password__container">
                     <h1>{props.title}</h1>
                     <p>{props.paragraph1} {props.obscuredEmail} {props.paragraph2}</p>
